cbb51a94cabf3be6601a9469363bcb732e1db7e2
Vinicius Luna <viniciuscluna@gmail.com>
2025-02-11 22:33:34 -0300

    feat: update page layout and styling - fix dark mode - update container styling - load script after div mounted (SPA) - remove unused styles (there are many more to remove)

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
-import "./App.css";
 import Footer from "./components/footer";
 import Header from "./components/header";
 import Sidebar from "./components/sidebar";
 import Dashboard from "./pages/dashboard";
 
 function App() {
-
   return (
     <>
-      <div className="page">
-        <Header />
-        <Sidebar />
-        <Dashboard />
-        <Footer /> 
-      </div>
+      <Header />
+      <Sidebar />
+      <Dashboard />
+      <Footer />
     </>
   );
 }
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,9 +4,9 @@ const Footer = () => {
       <div className="container">
         <span className="text-muted">
           Copyright © <span id="year">2025</span>
-          <a href="javascript:void(0);" className="text-dark fw-medium">Udon</a>.
+          <a href="#" className="text-dark fw-medium">Udon</a>.
           Designed with <span className="bi bi-heart-fill text-danger"></span> by
-          <a href="javascript:void(0);">
+          <a href="#">
             <span className="fw-medium text-primary text-decoration-underline">Spruko</span>
           </a>
           All rights reserved
