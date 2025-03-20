632b7cbac689a6bcae374ff0531d8c706f0b5094
Vinicius Luna <viniciuscluna@gmail.com>
2025-03-19 22:26:44 -0300

    feat: add wpp section

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,35 @@ import { createRoot } from "react-dom/client";
 import "./index.scss";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Dashboard from "./pages/dashboard.tsx";
+import Messages from "./pages/messages.tsx";
 
 // Create a client
 const queryClient = new QueryClient();
 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Dashboard />
+      },
+      {
+        path: '/messages',
+        element: <Messages />
+      }
+    ]
+  },
+]);
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </StrictMode>
 );
---
+++ src/pages/messages.tsx
@@ -0,0 +1,10 @@
+export const Messages = () => {
+    return (
+
+        <div className="main-content app-content">
+            <div className="container-fluid">dimiscosta</div>
+        </div>
+    )
+}
+
+export default Messages;
