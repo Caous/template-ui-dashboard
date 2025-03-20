9a7941b882c75bd7a49b331bdaaf058510503f45
Vinicius Luna <viniciuscluna@gmail.com>
2025-03-19 23:14:45 -0300

    feat: add whatsapp logic to show messages

--- src/pages/messages.tsx
+++ src/pages/messages.tsx
@@ -1,8 +1,56 @@
+
+// Interfaces para tipar os dados
+
+import { useQuery } from "@tanstack/react-query";
+import { getGroupMessages } from "../services/messageService";
+import { GroupMessage } from "../models/GroupMessage";
+import { useCallback, useMemo, useState } from "react";
+import Message from "../components/message";
+
+
+
 export const Messages = () => {
+
+    const [selectedMessage, setSelectedMessage] = useState<GroupMessage | undefined>(undefined);
+
+    const getLastMessagesQuery = useQuery({
+        queryKey: ["GroupMessages"],
+        queryFn: getGroupMessages,
+    });
+
+
+
     return (
 
         <div className="main-content app-content">
-            <div className="container-fluid">dimiscosta</div>
+            <div className="container-fluid">
+
+                <h1 className="mt-4">Mensagens</h1>
+
+                <div className="row">
+                    <div className="col-6">
+                        <h2>Conversas</h2>
+                        <div>
+                            {getLastMessagesQuery.data?.map((group, idx) => (
+                                <button
+                                    key={idx}
+                                    className="btn btn-primary mb-2"
+                                    onClick={() => setSelectedMessage(group)}
+                                >
+                                    {group.conversationId}
+                                </button>
+                            ))}
+                        </div>
+                    </div>
+
+                    <div className="col-6">
+                        <h2>Chat</h2>
+                        <div>
+                            <Message group={selectedMessage} />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/services/messageService.ts
+++ src/services/messageService.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
 import { WhatsappMessage } from "../models/WhatsAppMessage";
 import { BASE_URL } from "../utils/constants";
+import { GroupMessage } from "../models/GroupMessage";
 
 const API_URL = `${BASE_URL}/api/Whatsapp/GetLastMessagens`;
+
+const API_GROUP_URL = `${BASE_URL}/api/Whatsapp/GetAllMessagensMongo`;
 
 // Função para buscar as últimas mensagens do WhatsApp
 export const getLastMessages = async (): Promise<WhatsappMessage[]> => {
@@ -14,3 +17,13 @@ export const getLastMessages = async (): Promise<WhatsappMessage[]> => {
     throw error;
   }
 };
+
+export const getGroupMessages = async (): Promise<GroupMessage[]> => {
+  try {
+    const response = await axios.get<GroupMessage[]>(API_GROUP_URL);
+    return response.data;
+  } catch (error) {
+    console.error("Erro ao buscar as mensagens do WhatsApp:", error);
+    throw error;
+  }
+}
